208d3c9df4394825c74608810ede7c306c1d3ba9
khushi-gami <khushigami2406@gmail.com>
2026-08-24 16:30:28 +0530

    Final Python Project 1 Done!

--- project1.py
+++ project1.py
@@ -1,34 +1,35 @@
-import datetime # For getting the current year
-# print() & input() ------------------------------------------------------
-
+import datetime # For getting the current year..
 
 
 print("Welcome to the Intractive personal Data Collector!")
 
 print()
-
+ 
+# take input from user..
 name = input("Please enter your name : ")
 age = int(input("Please enter your age : "))
 height = float(input("Please enter your height in meters : "))
 favourite_number = int(input("Please enter your favourite number : "))
 
+# birth year calculation..
 current_year = datetime.datetime.now().year
 birth_year = int(current_year - age)
 
 print()
 
-print("Thank you! Here is the information we collected :")
+print("Thank you! Here is the information we collected : ")
 
 print()
 
-print(f"Name : {name}",type(name),"Memory Address :",id(name))
-print(f"Age : {age}",type(age),"Memory Address :",id(age))
-print(f"Height : {height}",type(height),"Memory Address :",id(height))
-print(f"Favourite Number : {favourite_number}",type(favourite_number),"Memory Address :",id(favourite_number))
+# print information given by the user , it's data types and memory location..
+print(f"Name : {name}",("Type : ",type(name),"Memory Address : ",id(name)))
+print(f"Age : {age}",("Type : ",type(age),"Memory Address : ",id(age)))
+print(f"Height : {height}",("Type :",type(height),"Memory Address : ",id(height)))
+print(f"Favourite Number : {favourite_number}",("Type :",type(favourite_number),"Memory Address :",id(favourite_number)))
 
 print()
 
-print(f"Your birth year is approximately : {birth_year} (Based on your age {age})")
+print(f"Your birth year is approximately : {birth_year} (Based on your of age {age})")
 
 print()
 
